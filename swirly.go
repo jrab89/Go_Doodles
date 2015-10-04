540089616d819fc63008cb3f65ce68f6f044d9fa
Jeff Rabovsky <jeffr@jellyvision.com>
2015-10-04 15:31:09 -0500

    'go run main.go swirly.go foo.png 800 600' works

--- swirly.go
+++ swirly.go
@@ -3,22 +3,28 @@ package main
 import "math"
 
 func r(x, y int) uint8 {
-	top := math.Hypot(float64(148-x), float64(1000-y)) + 1
-	bottom := math.Sqrt(float64(math.Abs(math.Sin((math.Sqrt(float64(sq(500-x)+sq(400-y))))/115.0)))) + 1
+	i := float64(x)
+	j := float64(y)
+
+	top := math.Hypot(148-i, 1000-j) + 1
+	bottom := math.Sqrt(float64(math.Abs(math.Sin((math.Hypot(500-i, 400-j))/115)))) + 1
 	return uint8(top / bottom)
 }
 
 func g(x, y int) uint8 {
-	// top := math.Sqrt(float64((148-x)*(148-x)+(1000-y)*(1000-y))) + 1
-	// bottom := math.Sqrt(float64(math.Abs(math.Sin((math.Sqrt(float64((500-x)*(500-x)+(400-y)*(400-y))))/115.0)))) + 1
-	// return uint8(top / bottom)
-	return 0
+	i := float64(x)
+	j := float64(y)
+
+	top := math.Hypot(610-i, 60-j) + 1
+	bottom := math.Sqrt(float64(math.Abs(math.Sin((math.Hypot(864-i, 860-j))/115)))) + 1
+	return uint8(top / bottom)
 }
 
 func b(x, y int) uint8 {
-	return 0
-}
+	i := float64(x)
+	j := float64(y)
 
-func sq(n int) int {
-	return n * n
+	top := math.Hypot(300-i, 20-j) + 1
+	bottom := math.Sqrt(float64(math.Abs(math.Sin((math.Hypot(503-i, 103-j))/115)))) + 1
+	return uint8(top / bottom)
 }
